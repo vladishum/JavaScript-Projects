9acf0f22f48f6535b0fcaa08cc222cd202f56d45
vladishum <112489778+vladishum@users.noreply.github.com>
2022-12-02 17:15:01 +0000

    Add files via upload

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,1 +1,10 @@
+function My_Second_Function() {
+    var str = "After CLICK";
+    var result = str.fontcolor("red");
+    document.getElementById("pgh1").innerHTML = result;
+}
 
+function My_Third_Function() {
+    var str = " Concatenating strings";
+    document.getElementById("hdg1").innerHTML += str;
+}

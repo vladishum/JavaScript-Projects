4a27b4d40cfc3a47732d69cde0eda784e87fd7cb
vladishum <112489778+vladishum@users.noreply.github.com>
2022-12-02 17:24:19 +0000

    Add files via upload

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,10 +1,12 @@
-function My_Second_Function() {
-    var str = "After CLICK";
-    var result = str.fontcolor("red");
-    document.getElementById("pgh1").innerHTML = result;
+function My_Second_Function() { // Defining a function and naming it
+    var str = "After CLICK";// Defining a variable and giving it a string value
+    var result = str.fontcolor("red");// Using fontcolor method on str variable
+    document.getElementById("pgh1").innerHTML = result; //Putting the value of result into the HTML element with "pgh1" id
 }
 
-function My_Third_Function() {
-    var str = " Concatenating strings";
-    document.getElementById("hdg1").innerHTML += str;
+function My_Third_Function() { // Defining a function and naming it
+    var str = " Concatenating strings"; // Defining a variable and giving it a string value
+    document.getElementById("hdg1").innerHTML += str;// Using the operator += to concatenate 
+                                                     //the str variable with the text inside the HTML 
+                                                     //element with id "hdg1"
 }

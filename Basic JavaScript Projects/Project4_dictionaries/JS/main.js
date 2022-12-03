967d676f8d36029b537c41e85d62fbd0f84188fd
vladishum <112489778+vladishum@users.noreply.github.com>
2022-12-03 19:58:02 +0000

    Add files via upload

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,1 +1,13 @@
-
+function myDictionary() { //defining funtion for the dictionary
+    var Vehicle = { //defining object with properties key-value pair
+        Type: "Car",
+        Model: "Toyota",
+        Color: "Red",
+        Fuel: "Petrol",
+        Engine: 1.4,
+        //Engine: 1.6
+    };
+    delete Vehicle.Color; //deleting key "Color"
+    document.getElementById("Dictionary").innerHTML = Vehicle.Engine;//displaying the value of the key "Engine"
+    document.getElementById("Dictionary2").innerHTML = Vehicle.Color;//dsiplaying the value of the key "Color" in this case it doesn't exist
+}
